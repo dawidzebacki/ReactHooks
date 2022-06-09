bb02c8639d344fa512a80fd1e08678558cea832a
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2022-06-09 12:06:24 +0200

    Create useState_13.jsx

---
+++ useState_13.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function WatchCount() {
+  const [count, setCount] = useState(0);
+
+  useEffect(function () {
+    setInterval(function log() {
+      console.log(`Count is: ${count}`);
+    }, 2000);
+  }, []);
+
+  const handleClick = () => setCount((count) => count + 1);
+  
+  return (
+    <>
+      <button onClick={handleClick}>Increase</button>
+      <div>Counter: {count}</div>
+    </>
+  );
+}
